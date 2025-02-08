030bbe251093666d36b5d263525d55d2bff41215
Mattzcap <matthewempeo@gmail.com>
2025-02-08 18:16:09 +0800

    for merging the login,singup and landing

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import LandingPage from "./pages/LandingPage";
+import SignupPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* Default Route - Login Page */}
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<LoginPage />} />
         
         {/* Landing Page Route */}
         <Route path="/landing" element={<LandingPage />} />
+
+        {/* Default Route - Login Page */}
+        <Route path="/signup" element={<SignupPage />} />
+
       </Routes>
     </Router>
   );
 }
+
+
 
 export default App;
