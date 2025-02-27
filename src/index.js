884dc7d873b82a7a79b8d064c7433aed8dab802f
Mattzcap <matthewempeo@gmail.com>
2025-02-28 00:28:07 +0800

    Removing the Basename

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // Only use HashRouter
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Use BrowserRouter for localhost, HashRouter for GitHub Pages
-const isLocalhost = window.location.hostname === "localhost";
-const Router = isLocalhost ? BrowserRouter : HashRouter;
-const basename = isLocalhost ? "/" : "/web-service"; // Set basename only for GitHub Pages
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename={basename}>
+    <HashRouter>  {/* Removed basename, HashRouter handles it automatically */}
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
 
