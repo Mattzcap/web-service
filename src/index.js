93292de917af71e3a3051108dc5f8a3f3c8b8b77
Mattzcap <matthewempeo@gmail.com>
2025-02-27 23:25:35 +0800

    FIXED the index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Detect environment: Use BrowserRouter for localhost, HashRouter for GitHub Pages
+// Ensure HashRouter is used for GitHub Pages to avoid blank pages
 const isLocalhost = window.location.hostname === "localhost";
 const Router = isLocalhost ? BrowserRouter : HashRouter;
-const basename = isLocalhost ? "/" : "/web-service";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename={basename}>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
